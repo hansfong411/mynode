07f20f9ba1a9f5b3d930e64f26a2b602ac3491ca
hansfong411 <hansfong411@gmail.com>
2021-07-19 15:47:17 +0800

    Find reason why failed to build mater branch due to the master branch has been moved to main branch

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 console.log("I love this game")
-console.log("Why mater branches not found")
-
+console.log("Why mater branch not found?")
+console.log("This is because master is no longer existed. We shall not have slave people in realworld")
+console.log("We just need slave devices")
